6f9d88d3fe4d2878da4a58d15f89dbfe823a134e
Niklas Sauer <nik.sauer@me.com>
2020-01-05 22:08:57 +0100

    simplify truffle config

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -1,14 +1,6 @@
-const Web3 = require("web3");
-
 const deployerAddress = "0x41c1c3d1f21a46ab84e4535167044676c30875be";
 
 module.exports = {
-    // Uncommenting the defaults below 
-    // provides for an easier quick-start with Ganache.
-    // You can also follow this format for other networks;
-    // see <http://truffleframework.com/docs/advanced/configuration>
-    // for more details on how to specify configuration options!
-
     networks: {
         local: {
             host: "blockchain",
